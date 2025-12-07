4db3694cefa920982b79354601f351385962b304
Pedro Lugon <pedromotta20@gmail.com>
2025-12-07 20:33:49 -0300

    feat: show api data in `task card`

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.css'
+  styleUrl: './task-card.component.css',
 })
 export class TaskCardComponent {
-
+  @Input() title = '';
+  @Input() limitDate!: null | string;
+  @Input() description!: null | string;
 }
